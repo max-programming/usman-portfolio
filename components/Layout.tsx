eac25bd5891ec977a7b48f28bafeffd3f01a3886
max-programming <ui.sabuwala@gmail.com>
2021-02-12 10:26:03 +0530

    🔍 Updated the head tag for SEO

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -13,6 +13,41 @@ export default function Layout({ title, children }: LayoutProps) {
     <div className="main-container">
       <Head>
         <title>{title} | Usman Sabuwala</title>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="HandheldFriendly" content="True" />
+        <meta name="MobileOptimized" content="320" />
+        <meta
+          name="description"
+          content="Personal site for Usman Sabuwala, Developer, Student, and Content Creator."
+        />
+        {/* <meta name="image" content=""/> */}
+
+        <meta property="og:title" content="Usman Sabwuala" />
+        <meta property="og:type" content="portfolio" />
+        <meta property="og:url" content="https://usman-s.me" />
+        {/* <meta property="og:image" content="" /> */}
+        <meta
+          name="og:description"
+          content="Personal site for Usman Sabuwala, Developer, Student, and Content Creator."
+        />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:creator" content="@MaxProgramming1" />
+        <meta name="twitter:title" content={title} />
+        <meta
+          name="twitter:description"
+          content="Personal site for Usman Sabuwala, Developer, Student, and Content Creator."
+        />
+        {/* <meta name="twitter:image" content="" /> */}
+
+        <link rel="canonical" href="https://usman-s.me" />
+
+        <meta
+          name="keywords"
+          content="usman, usman sabuwala, max programming, max, usman website, usman's website, max programming website, usman sabuwala website, usman portfolio"
+        />
       </Head>
       <Header />
       {/* <Nav /> */}
